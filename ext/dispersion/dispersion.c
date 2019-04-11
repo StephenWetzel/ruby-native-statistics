ca4edb30a9bea61aa57fc4534b4d196a2f4f60db
Stephen Wetzel <swetzel@inspireenergy.com>
2019-04-11 13:32:43 -0400

    Fix if

--- ext/dispersion/dispersion.c
+++ ext/dispersion/dispersion.c
@@ -47,11 +47,12 @@ VALUE sort_array(VALUE array, unsigned long array_length) {
 
   for(ii = 0; ii < array_length - 1; ii++) {
     for(jj = 0; jj < array_length - ii - 1; jj++) {
-       if(rb_num2dbl(rb_ary_entry(array, jj)) > rb_num2dbl(rb_ary_entry(array, jj + 1)))
-          // swap(array, jj, jj + 1);
-          t = rb_ary_entry(array, jj);
-          rb_ary_store(array, jj, rb_ary_entry(array, jj + 1));
-          rb_ary_store(array, jj + 1, t);
+      if(rb_ary_entry(array, jj) > rb_ary_entry(array, jj + 1)) {
+        // swap(array, jj, jj + 1);
+        t = rb_ary_entry(array, jj);
+        rb_ary_store(array, jj, rb_ary_entry(array, jj + 1));
+        rb_ary_store(array, jj + 1, t);
+      }
     }
   }
   return array;
