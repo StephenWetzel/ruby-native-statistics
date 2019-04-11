c396db3f36479f37b86cfa635977719615321fea
Stephen Wetzel <swetzel@inspireenergy.com>
2019-04-11 12:22:38 -0400

    Add median

--- ext/dispersion/dispersion.c
+++ ext/dispersion/dispersion.c
@@ -9,6 +9,7 @@ void Init_dispersion() {
   rb_define_method(DispersionModule, "var", rb_sample_variance, 0);
   rb_define_method(DispersionModule, "varp", rb_population_variance, 0);
   rb_define_method(DispersionModule, "mean", rb_mean, 0);
+  rb_define_method(DispersionModule, "median", rb_median, 0);
 }
 
 double calculate_mean(VALUE array, unsigned long array_length){
@@ -23,6 +24,31 @@ double calculate_mean(VALUE array, unsigned long array_length){
   mean = total / array_length;
 
   return mean;
+}
+
+double calculate_median(VALUE array, unsigned long array_length){
+  int n;
+  sort_array(array);
+  n = (array_length + 1) / 2 - 1
+  return array[n]
+}
+
+void swap(int *p, int *q) {
+   int t;
+   t = *p;
+   *p = *q;
+   *q = t;
+}
+
+VALUE sort_array(VALUE array, unsigned long array_length){
+   int ii, jj, temp;
+
+   for(ii = 0; ii < n-1; ii++) {
+      for(jj = 0; jj < n-ii-1; jj++) {
+         if(a[jj] > a[jj+1])
+            swap(&a[jj], &a[jj+1]);
+      }
+   }
 }
 
 double calculate_total_distance_from_mean(VALUE array, unsigned long array_length){
@@ -107,5 +133,19 @@ VALUE rb_mean(VALUE self) {
   }
 
   return DBL2NUM(calculate_mean(self, array_length));
+}
+
+VALUE rb_median(VALUE self) {
+  unsigned int array_length;
+
+  Check_Type(self, T_ARRAY);
+
+  array_length = rb_long2int(RARRAY_LEN(self));
+
+  if (array_length <= 0) {
+    rb_raise(rb_eRangeError, "array must have at least one element");
+  }
+
+  return DBL2NUM(calculate_median(self, array_length));
 }
 
